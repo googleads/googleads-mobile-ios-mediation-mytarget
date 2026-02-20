676b295678585d7ff2971d515c86f11a447a23b6
Aldo Becerril <aldobecerril@google.com>
2026-02-20 22:57:34 +0000

    Updating MyTarget iOS adapter for release version 5.40.0.
    PiperOrigin-RevId: 873086895

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/myTargetSDK/mytarget-ios-spm.git",
-      exact: "5.39.1"
+      exact: "5.40.0"
     ),
     .package(
       url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
@@ -47,7 +47,7 @@ let package = Package(
     ),
     .binaryTarget(
       name: "MyTargetAdapter",
-      url: "https://dl.google.com/googleadmobadssdk/mediation/ios/mytarget/MyTargetAdapter-5.39.1.0.zip",
+      url: "https://dl.google.com/googleadmobadssdk/mediation/ios/mytarget/MyTargetAdapter-5.40.0.0.zip",
       checksum: "66a22abcfdf0d60ec67ae2e364029e69887ffe3d04770339d337e9ab19b7c96d"
     ),
   ]
